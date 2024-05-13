8d834eb55c6480ef16c1a60bae987a6f68fe0420
Artuom4ik <masterskayaigr123@gmail.com>
2024-05-13 12:35:42 +0300

    Write collection route

--- planX_project/urls.py
+++ planX_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('links/', include('links.urls')),
+    path('collections/', include('collection.urls')),
 ]
