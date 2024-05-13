3aa464b6a239643c86edf1fa54199dca62bd3ea9
Artuom4ik <masterskayaigr123@gmail.com>
2024-05-13 10:23:56 +0300

    Add Update- and ShowLink serializers

--- links/serializator.py
+++ links/serializator.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.utils import timezone
 
 from .models import Link
 
@@ -9,10 +10,10 @@ from .models import Link
 class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
-        fields = ['url']
+        fields = ['link',]
 
     def create(self, validated_data):
-        url = validated_data.get('url')
+        url = validated_data.get('link')
 
         response = requests.get(url)
         response.raise_for_status()
@@ -30,14 +31,40 @@ class LinkSerializer(serializers.ModelSerializer):
 
         if not fields["og:description"]:
             fields["og:description"] = soup.find('meta', name='description')['content']
-
+        
         link = Link.objects.get_or_create(
             title=fields["og:title"],
             short_description=fields["og:description"],
             link=url,
             image=fields["og:image"],
             link_type=fields["og:type"],
-            user=self.request.user
+            user=self.context['request'].user
         )
 
         return link
+
+
+class UpdateLinkSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=100, required=False)
+    short_description = serializers.CharField(max_length=200, required=False)
+    link = serializers.CharField(max_length=100, required=False)
+    class Meta:
+        model = Link
+        fields = ["id", "title", "short_description", "link", "image", "link_type"]
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.short_description = validated_data.get("short_description", instance.short_description)
+        instance.link = validated_data.get("link", instance.link)
+        instance.image = validated_data.get("image", instance.image)
+        instance.link_type = validated_data.get("link_type", instance.link_type)
+        instance.update_at = timezone.now()
+        instance.save()
+
+        return instance
+
+
+class ShowLinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = '__all__'
