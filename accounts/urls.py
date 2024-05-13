fa811efdb07d0e37817e9c38272578a04d229793
Artuom4ik <masterskayaigr123@gmail.com>
2024-05-13 19:40:00 +0300

    Connect django-rest-passwordreset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,22 @@
-from django.urls import path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from django.urls import path, include
 
 from accounts.views import *
 
 
 urlpatterns = [
     path('login/', AuthViewSet.as_view({'post': 'login'}), name='login'),
-    path('register/', AuthViewSet.as_view({'post': 'register'}), name='register'),
-    path('changepassword/', AuthViewSet.as_view({'post': 'change_password'}), name='change_password'),
+    path(
+        'register/',
+        AuthViewSet.as_view({'post': 'register'}),
+        name='register'
+    ),
+    path(
+        'change_password/',
+        AuthViewSet.as_view({'post': 'change_password'}),
+        name='change_password'
+    ),
     path('logout/', AuthViewSet.as_view({'post': 'logout'}), name='logout'),
+    path(
+        'api/password_reset/',
+        include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
--- planX_project/settings.py
+++ planX_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_jwt',
+    'django_rest_passwordreset',
 ]
 
 JWT_AUTH = {
